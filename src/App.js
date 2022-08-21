69ab574c21b60aeb5630a65714eed8d686012d0c
Zohaib R <zohaibr123@hotmail.com>
2022-08-21 17:47:56 -0400

    counter app with increment decrement and reset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+
+  function Increment(){
+  setCount(count+1);
+}
+
+const Decrement = () => setCount(count-1);
+
+const Reset = () =>setCount(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="counter">
+      <h1 className = "heading">Counter</h1>
+      <p className = "count">{count}</p>
+      <button className = " btn increase" type = "submit" onClick = {Increment}>+</button>
+      <button className = "btn decrease"  type = "submit" onClick = {Decrement}>-</button>
+      <button className = "reset"  type = "submit" onClick = {Reset}>Reset</button>
+      </div>
   );
 }
 
